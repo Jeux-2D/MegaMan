511b9958b804e61e3bc399f237be0a107d2e1a9e
Just1nBon1n <152336193+Just1nBon1n@users.noreply.github.com>
2024-03-21 11:13:24 -0400

    Partie 2 debut et fin

--- Assets/Scripts/ControlerMegaMan.cs
+++ Assets/Scripts/ControlerMegaMan.cs
@@ -14,61 +14,75 @@ public class ControlerPersonnage : MonoBehaviour
     float vitesseY;      //vitesse verticale 
     public float vitesseSaut;   //vitesse de saut désirée
 
+    public AudioClip sonMort;   //Son de mort de Megaman
+
+    private bool estMort;   //Variable qui determine si Megaman est mort
+
     /* Détection des touches et modification de la vitesse de déplacement;
        "a" et "d" pour avancer et reculer, "w" pour sauter
     */
     void Update()
     {
-        // déplacement vers la gauche
-        if (Input.GetKey("a"))
+        if (!estMort)
         {
-            vitesseX = -vitesseXMax;
-            GetComponent<SpriteRenderer>().flipX = true;
+            // déplacement vers la gauche
+            if (Input.GetKey("a"))
+            {
+                vitesseX = -vitesseXMax;
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+            else if (Input.GetKey("d"))   //déplacement vers la droite
+            {
+                vitesseX = vitesseXMax;
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+            else
+            {
+                vitesseX = GetComponent<Rigidbody2D>().velocity.x;  //vitesse actuelle horizontale
+            }
+
+            // sauter l'objet à l'aide la touche "w"
+            if (Input.GetKeyDown("w") && Physics2D.OverlapCircle(transform.position, 0.25f))
+            {
+                vitesseY = vitesseSaut;
+                GetComponent<Animator>().SetBool("saut", true);
+            }
+            else
+            {
+                vitesseY = GetComponent<Rigidbody2D>().velocity.y;  //vitesse actuelle verticale
+            }
+
+            //Applique les vitesses en X et Y
+            GetComponent<Rigidbody2D>().velocity = new Vector2(vitesseX, vitesseY);
+
+
+            //**************************Gestion des animaitons de course et de repos********************************
+            //Active l'animation de course si la vitesse de déplacement n'est pas 0, sinon le repos sera jouer par Animator
+            if (vitesseX > 0.1f || vitesseX < -0.1f)
+            {
+                GetComponent<Animator>().SetBool("marche", true);
+            }
+            else
+            {
+                GetComponent<Animator>().SetBool("marche", false);
+            }
         }
-        else if (Input.GetKey("d"))   //déplacement vers la droite
-        {
-            vitesseX = vitesseXMax;
-            GetComponent<SpriteRenderer>().flipX = false;
-        }
-        else
-        {
-            vitesseX = GetComponent<Rigidbody2D>().velocity.x;  //vitesse actuelle horizontale
-        }
-
-        // sauter l'objet à l'aide la touche "w"
-        if (Input.GetKeyDown("w"))
-        {
-            vitesseY = vitesseSaut;
-            GetComponent<Animator>().SetBool("saut", true);
-        }
-        else
-        {
-            vitesseY = GetComponent<Rigidbody2D>().velocity.y;  //vitesse actuelle verticale
-        }
-
-        //Applique les vitesses en X et Y
-        GetComponent<Rigidbody2D>().velocity = new Vector2(vitesseX, vitesseY);
-
-
-        //**************************Gestion des animaitons de course et de repos********************************
-        //Active l'animation de course si la vitesse de déplacement n'est pas 0, sinon le repos sera jouer par Animator
-        if (vitesseX > 0.1f || vitesseX < -0.1f)
-        {
-            GetComponent<Animator>().SetBool("marche", true);
-        }
-        else
-        {
-            GetComponent<Animator>().SetBool("marche", false);
-        }
-
-
-
-
-
     }
-    void OnCollisionEnter2D(Collision2D collision)
+
+    void OnCollisionEnter2D(Collision2D infoCollision)
     {
-        GetComponent<Animator>().SetBool("saut", false);
+        //Animation saut suelement si objet en dessous
+        if (Physics2D.OverlapCircle(transform.position, 0.25f))
+        {
+            GetComponent<Animator>().SetBool("saut", false);
+        }
+
+        //Collision avec la roue
+        if (infoCollision.gameObject.name == "RoueDentelee")
+        {
+            GetComponent<Animator>().SetBool("mort", true);
+            GetComponent<AudioSource>().PlayOneShot(sonMort, 1f);
+        }
 
     }
 }
